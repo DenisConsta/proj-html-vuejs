d100236f822b094a4485d408ad11b7d20b809166
DenisConsta <denisconstantin338@gmail.com>
2022-11-24 17:16:10 +0100

    [add] header HTML content (AppHeader & AppButton)

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,18 +1,83 @@
 <script>
+
+import AppButton from './partials/AppButton.vue';
+
 export default {
   name: "AppHeader",
+  components: {
+    AppButton,
+
+  }
 };
 </script>
 
 <template>
+  <!--?= Header -->
   <header>
     <div class="container">
-      <h3>Header</h3>
-    </div>  
+      <!-- ? Nav -->
+      <nav>
+        <!--? Logo -->
+        <div class="logo d-flex align-items-center">
+          <img src="../../src/assets/images/logo-2x.png" alt="" />
+        </div>
+
+        <!-- ? nav-items -->
+        <div class="nav-items d-flex align-items-center">
+          <!-- ? Links -->
+          <div class="links">
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Services</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Videos</a></li>
+              <li><a href="#">Blog</a></li>
+              <li><a href="#">Store</a></li>
+            </ul>
+          </div>
+
+          <!-- ? Button -->
+          <AppButton text="schedule a workout"/>
+
+        </div>
+      </nav>
+    </div>
   </header>
 </template>
 
 <style lang="scss" scoped>
 @use "../styles/partials/variables" as *;
 @use "../styles/partials/mixin" as *;
+
+header {
+  background-color: $color-codGrey;
+  color: white;
+
+
+  nav {
+    padding: 1.6rem 0;
+    @include flexMC("between", "center");
+
+    .logo {
+      
+      img {
+        max-height: 2.5rem;
+        max-width: 14rem;
+      }
+    }
+
+    .nav-items{
+      gap: 2rem;
+      @include flexMC("center", "center");
+      
+      ul {
+      @include reset-list;
+      @include flexMC("center", "center");
+      gap: 2rem;
+    }
+    }
+  
+
+  }
+}
 </style>
---
+++ src/components/partials/AppButton.vue
@@ -0,0 +1,30 @@
+
+<script>
+export default {
+  name: 'AppButton',
+  props:{
+    text: String,
+
+  }
+}
+</script>
+
+<template>
+
+  <button> {{text}} </button>
+
+</template>
+
+<style lang="scss" scoped>
+@use '../../styles/partials/variables' as *;
+
+button{
+  border: none;
+  background-color: $color-dodgerBlue;
+  color: white;
+  border-radius: 1.2rem;
+  padding: .8rem 1.6rem;
+}
+
+
+</style>
