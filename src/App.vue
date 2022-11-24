1a189bcbde391b2bf78daa3dfc6ed041edbe431d
DenisConsta <denisconstantin338@gmail.com>
2022-11-24 12:13:32 +0100

    [init] first commit - scaffolding

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  name: 'App',
+  components:{
+    AppHeader,
+
+  }
+}
+</script>
+
+<template>
+  <AppHeader/>
+  <div class="container">
+    <h1 class="display-1">Hello World</h1>
+  </div>
+</template>
+
+<style lang="scss">
+@use './styles/general.scss'
+
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: 'AppHeader',
+  
+}
+</script>
+
+<template>
+  <header>Header</header>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixin' as *;
+
+
+</style>
